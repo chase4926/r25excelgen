00dbad0b497b46b1ebc3276d26c30565188c78f4
Chase Arnold <chase4926@gmail.com>
2017-02-27 14:01:11 -0500

    Half way through gui

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from datetime import time
 from datetime import *
 from collections import defaultdict
 import re
+import cocos
 
 
 def format_time(t):
@@ -33,6 +34,18 @@ def time_between(t1, t2):
 def unicode_to_time(u):
   new_time = datetime.strptime(u.encode('ASCII', 'ignore'), '%I:%M %p')
   return time(new_time.hour, new_time.minute, new_time.second)
+
+def resource_common_name(resource):
+  return {
+    'Laptop wifi': 'Laptop',
+    'Computer / Dell Laptop': 'Laptop',
+    'Laptop Wireless Cart #1 (20)': 'Cart',
+    'Laptop Wireless Cart #2 (20)': 'Cart',
+    'Laptop Wireless Cart #3 (20)': 'Cart',
+    'Clickers 25': 'Clickers',
+    'Clickers 52': 'Clickers-50',
+    'Wireless Presenter': 'Presenter',
+  }.get(resource, '')
 
 class Event:
   def __init__(self):
@@ -94,6 +107,26 @@ class Event:
     else:
       return False
   
+  def get_pickup_str(self):
+    if len(self.pickup_window) == 1:
+      if self.pickup_window[0] == 'OPEN':
+        time = 'OPEN'
+      else:
+        time = "@%s" % format_time(self.pickup_window[0])
+    else:
+      time = "%s-%s" % (format_time(self.pickup_window[0]), format_time(self.pickup_window[1]))
+    return "%s | %s | %s" % (time, self.space, ",".join([resource_common_name(a) for a in self.resource]))
+  
+  def get_delivery_str(self):
+    if len(self.delivery_window) == 1:
+      if self.delivery_window[0] == 'OPEN':
+        time = 'OPEN'
+      else:
+        time = "@%s" % format_time(self.delivery_window[0])
+    else:
+      time = "%s-%s" % (format_time(self.delivery_window[0]), format_time(self.delivery_window[1]))
+    return "%s | %s | %s" % (time, self.space, ",".join([resource_common_name(a) for a in self.resource]))
+  
   def __repr__(self):
     return "%s - %s, %s, %s" % (format_time(self.start), format_time(self.end), self.space, self.resource)
 
@@ -121,7 +154,7 @@ class EventBook:
             event = Event()
             event.set_start(row[0].value) # Start datetime.time
             event.set_end(row[1].value) # End datetime.time
-            event.space = row[5].value.encode('ASCII', 'ignore') # Space
+            event.space = self.format_space(row[5].value.encode('ASCII', 'ignore')) # Space
             if row[6].value != None:
               event.add_resource(row[6].value.encode('ASCII', 'ignore')) # Resource
             rooms[event.space].append(event)
@@ -244,7 +277,7 @@ class EventBook:
     i = 2 # Row to start on
     for event in self.reservations:
       # Write event details to row
-      template["B%i" % i] = self.format_space(event.space)
+      template["B%i" % i] = event.space
       template["K%i" % i] = format_time(event.start)
       template["M%i" % i] = format_time(event.end)
       for resource in event.resource:
@@ -267,64 +300,130 @@ class EventBook:
     
     template_book.save("%s.xlsx" % (datetime.now() + timedelta(days=1)).strftime("%b-%d"))
 
-
-#def count_events(rooms):
-  #i = 0
-  #for room in rooms:
-    #for event in rooms[room]:
-      #i += 1
-  #return i
-
-
-def get_current_deliveries(reservations, td=2):
-  # Returns a list of tuples. (td = time difference in hours)
-  # tuples contain (event, priority) where priority is 1-100 with 100 being urgent
-  current_time = datetime.now().time()#.replace(hour=10).replace(minute=31)
-  result = list()
-  for event in reservations:
-    if len(event.delivery_window) == 1:
-      # Either OPEN or @
-      if event.delivery_window[0] == 'OPEN':
-        # Deliver before event.start
-        if current_time < event.start and time_between(current_time, event.start) < timedelta(hours=td):
-          result.append( (event, 1) ) # FIXME: Priority
+  def get_current_deliveries(self, current_time, td=2):
+    # Returns a list of tuples. (td = time difference in hours)
+    # tuples contain (event, priority) where priority is 1-100 with 100 being urgent
+    result = list()
+    for event in self.reservations:
+      if len(event.delivery_window) == 1:
+        # Either OPEN or @
+        if event.delivery_window[0] == 'OPEN':
+          # Deliver before event.start
+          if current_time < event.start and time_between(current_time, event.start) < timedelta(hours=td):
+            result.append( (event, 1) ) # FIXME: Priority
+        else:
+          # Deliver right at [0]
+          if current_time < event.delivery_window[0] and time_between(current_time, event.delivery_window[0]) < timedelta(minutes=30):
+            result.append( (event, 1) ) # FIXME: Priority
       else:
-        # Deliver right at [0]
-        if current_time < event.delivery_window[0] and time_between(current_time, event.delivery_window[0]) < timedelta(minutes=30):
+        # Deliver after [0] and before [1]
+        if current_time > event.delivery_window[0] and current_time < event.delivery_window[1] and time_between(current_time, event.delivery_window[1]) < timedelta(hours=td):
           result.append( (event, 1) ) # FIXME: Priority
-    else:
-      # Deliver after [0] and before [1]
-      if current_time > event.delivery_window[0] and current_time < event.delivery_window[1]:
-        result.append( (event, 1) ) # FIXME: Priority
-  return result
-
-
-def get_current_pickups(reservations, td=2):
-  # Returns a list of tuples. (td = time difference in hours)
-  # tuples contain (event, priority) where priority is 1-100 with 100 being urgent
-  current_time = datetime.now().time().replace(hour=9).replace(minute=28)
-  result = list()
-  for event in reservations:
-    if len(event.pickup_window) == 1:
-      #Either OPEN or @
-      if event.pickup_window[0] == 'OPEN':
-        # Pickup after event.end
-        if current_time > event.end and time_between(event.end, current_time) < timedelta(hours=td):
-          result.append( (event, 1) ) # FIXME: Priority
+    return result
+  
+  def get_current_pickups(self, current_time, td=2):
+    # Returns a list of tuples. (td = time difference in hours)
+    # tuples contain (event, priority) where priority is 1-100 with 100 being urgent
+    result = list()
+    for event in self.reservations:
+      if len(event.pickup_window) == 1:
+        #Either OPEN or @
+        if event.pickup_window[0] == 'OPEN':
+          # Pickup after event.end
+          if current_time > event.end and time_between(event.end, current_time) < timedelta(hours=td):
+            result.append( (event, 1) ) # FIXME: Priority
+        else:
+          # Pickup right at [0]
+          if current_time < event.pickup_window[0] and time_between(current_time, event.pickup_window[0]) < timedelta(minutes=30):
+            result.append( (event, 1) ) # FIXME: Priority
       else:
-        # Pickup right at [0]
-        if current_time < event.pickup_window[0] and time_between(current_time, event.pickup_window[0]) < timedelta(minutes=30):
+        # Pickup after [0] and before [1]
+        if current_time > event.pickup_window[0] and current_time < event.pickup_window[1]:
           result.append( (event, 1) ) # FIXME: Priority
-    else:
-      # Pickup after [0] and before [1]
-      if current_time > event.pickup_window[0] and current_time < event.pickup_window[1]:
-        result.append( (event, 1) ) # FIXME: Priority
-  return result
+    return result
 
 
-wb = EventBook()
-wb.load_workbook('reservations.xlsx')
-wb.save_workbook()
+# Gui Code
 
+class EventWindow(cocos.layer.Layer):
+  def __init__(self):
+    super(EventWindow, self).__init__()
+    self.wb = EventBook()
+    self.wb.load_workbook('reservations.xlsx')
+    self.font = 'Arial'
+    # Deliveries
+    delivery_label = cocos.text.Label(
+      'Deliveries',
+      font_name = self.font,
+      font_size = 24,
+      anchor_x = 'center', anchor_y = 'center'
+    )
+    delivery_label.position = 100, 690
+    self.add(delivery_label)
+    # Pickups
+    pickup_label = cocos.text.Label(
+      'Pickups',
+      font_name = self.font,
+      font_size = 24,
+      anchor_x = 'center', anchor_y = 'center'
+    )
+    pickup_label.position = 1190, 690
+    self.add(pickup_label)
+    # Updated at ...
+    self.time_label = cocos.text.Label(
+      'Not updated!',
+      font_name = self.font,
+      font_size = 24,
+      anchor_x = 'center', anchor_y = 'center'
+    )
+    self.time_label.position = 640, 690
+    self.add(self.time_label)
+    # Delivery labels
+    self.delivery_slots = list()
+    for i in range(25):
+      label = cocos.text.Label(
+        '',
+        font_name = self.font,
+        font_size = 14,
+        anchor_x = 'left', anchor_y = 'center'
+      )
+      label.position = 20, 640 - i*24
+      self.delivery_slots.append(label)
+      self.add(label)
+    # Pickup labels
+    self.pickup_slots = list()
+    for i in range(25):
+      label = cocos.text.Label(
+        '',
+        font_name = self.font,
+        font_size = 14,
+        anchor_x = 'right', anchor_y = 'center'
+      )
+      label.position = 1260, 640 - i*24
+      self.pickup_slots.append(label)
+      self.add(label)
+    self.update_events()
+  
+  def update_events(self):
+    current_time = datetime.now().time().replace(hour=10).replace(minute=31)
+    self.time_label.element.text = "Updated at %s" % format_time(current_time)
+    # Clear old text
+    for label in self.delivery_slots:
+      label.element.text = ''
+    for label in self.pickup_slots:
+      label.element.text = ''
+    # Add new text
+    for i, event in enumerate(self.wb.get_current_deliveries(current_time)):
+      self.delivery_slots[i].element.text = event[0].get_delivery_str()
+    for i, event in enumerate(self.wb.get_current_pickups(current_time)):
+      self.pickup_slots[i].element.text = event[0].get_pickup_str()
+
+# ---
+
+#wb.save_workbook()
+
+cocos.director.director.init(width=1280, height=720, caption="R25 Excel Gen")
+main_scene = cocos.scene.Scene(EventWindow())
+cocos.director.director.run(main_scene)
 
 
